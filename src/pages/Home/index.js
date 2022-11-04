858aeea7e95c82cbd723de86a1ec0d15afd04764
Stefan Jaspers <54743154+stefanjaspers@users.noreply.github.com>
2022-11-04 19:43:30 +0100

    changes

---
+++ src/pages/Home/index.js
@@ -0,0 +1,76 @@
+// @mui material components
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+// import Card from "@mui/material/Card";
+
+// Material Kit 2 React components
+import MKBox from "components/MKBox";
+import MKTypography from "components/MKTypography";
+
+// Material Kit 2 React examples
+import DefaultNavbar from "examples/Navbars/DefaultNavbar";
+
+// Routes
+import routes from "routes";
+
+// Images
+import bgImage from "assets/images/bg-presentation.jpg";
+
+function Presentation() {
+  return (
+    <>
+      <DefaultNavbar
+        routes={routes}
+        action={{
+          type: "external",
+          route: "https://www.creative-tim.com/product/material-kit-react",
+          label: "free download",
+          color: "info",
+        }}
+        // transparent
+        dark
+      />
+      <MKBox
+        minHeight="100vh"
+        width="100%"
+        sx={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "top",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Container>
+          <Grid container item xs={12} lg={7} justifyContent="center" mx="auto">
+            <MKTypography
+              variant="h1"
+              color="white"
+              mt={-6}
+              mb={1}
+              sx={({ breakpoints, typography: { size } }) => ({
+                [breakpoints.down("md")]: {
+                  fontSize: size["3xl"],
+                },
+              })}
+            >
+              Stroke Predictor{" "}
+            </MKTypography>
+            <MKTypography
+              variant="body1"
+              color="white"
+              textAlign="center"
+              px={{ xs: 6, lg: 12 }}
+              mt={1}
+            >
+              An application that is able to predict whether you are at risk of having a stroke in
+              the future.
+            </MKTypography>
+          </Grid>
+        </Container>
+      </MKBox>
+    </>
+  );
+}
+
+export default Presentation;
